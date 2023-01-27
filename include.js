9bb6c0cdf30e121854cad48f028397d4fafa5685
MEO-SMR <117665794+MEO-SMR@users.noreply.github.com>
2023-01-27 11:10:29 +0800

    Update include.js

--- include.js
+++ include.js
@@ -1,10 +1,12 @@
     $(document).ready(function(){
       $.ajax({
-          url: 'katawan.html',
-          type: 'GET',
-          success: function(data) {
-              $("#katawan").html(data);
-          }
+        url: "katawan.html",
+        type: "GET",
+        success: function(data){
+          $("#katawan").html(data);
+        },
+        error: function(xhr, status, error){
+          console.log("Error: " + error);
+        }
       });
     });
- 
