c3808d661c25f12e07ed665ed0a2566d0e84b9ec
MEO-SMR <117665794+MEO-SMR@users.noreply.github.com>
2023-01-27 09:19:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -16,10 +16,10 @@
         if (document.getElementById("username").value == username &&   document.getElementById("password").value == password) {
 	   alert('Validation succeeded, select ok to continue..', '', 'height=200,width=200,top=' + (window.innerHeight / 2 - 100) + ',left=' + (window.innerWidth / 2 - 100));
 	   window.location.href = url;
-        } else  if (document.getElementById("username").value == "" && document.getElementById("password").value != "") {
+        } else  if (document.getElementById("username").value == "") {
              document.getElementById('username').focus();
 	     alert('Please enter username..', '', 'height=200,width=200,top=' + (window.innerHeight / 2 - 100) + ',left=' + (window.innerWidth / 2 - 100));
-	} else  if (document.getElementById("username").value != "" && document.getElementById("password").value == "") {
+	} else  if (document.getElementById("password").value == "") {
 	     document.getElementById('password').focus();
 	     alert('Please enter password..', '', 'height=200,width=200,top=' + (window.innerHeight / 2 - 100) + ',left=' + (window.innerWidth / 2 - 100));
         } else {
