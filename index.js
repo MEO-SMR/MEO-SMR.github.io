a8a792cdd54cad259de6a718faa5bc88af842ca8
MEO-SMR <117665794+MEO-SMR@users.noreply.github.com>
2023-01-27 09:23:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -16,16 +16,16 @@
         if (document.getElementById("username").value == username &&   document.getElementById("password").value == password) {
 	   alert('Validation succeeded, select ok to continue..', '', 'height=200,width=200,top=' + (window.innerHeight / 2 - 100) + ',left=' + (window.innerWidth / 2 - 100));
 	   window.location.href = url;
-        } else  if (document.getElementById("username").value == "") {
+        } else  if (document.getElementById("username").value == "" && document.getElementById("password").value != 0) {
              document.getElementById('username').focus();
 	     alert('Please enter username..', '', 'height=200,width=200,top=' + (window.innerHeight / 2 - 100) + ',left=' + (window.innerWidth / 2 - 100));
-	} else  if (document.getElementById("password").value == "") {
+	} else  if (document.getElementById("password").value == "" && document.getElementById("username").value != 0) {
 	     document.getElementById('password').focus();
 	     alert('Please enter password..', '', 'height=200,width=200,top=' + (window.innerHeight / 2 - 100) + ',left=' + (window.innerWidth / 2 - 100));
         } else {
 	    document.getElementById('password').value = "";
 	    document.getElementById('password').focus();
-	     alert('Wrong Password!', '', 'height=200,width=200,top=' + (window.innerHeight / 2 - 100) + ',left=' + (window.innerWidth / 2 - 100));	
+	     alert('Wrong Username or Password!', '', 'height=200,width=200,top=' + (window.innerHeight / 2 - 100) + ',left=' + (window.innerWidth / 2 - 100));	
 	}
       });
 
